0ca38f72874a37ef69d9c0b4962f22cbe820da08
housamhegazy <geohousam@gmail.com>
2025-09-12 03:24:05 +0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+//هاستدعي ال مونجوز للربط بين مشروعي والداتابيز
+const mongoose = require("mongoose");
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html",{root:__dirname})
-})
 // app.post('/', (req, res) => {
 //   res.send('Got a POST request')
 // })
@@ -16,5 +14,19 @@ app.get('/', (req, res) => {
 //   res.send('Got a DELETE request at /user')
 // })
 app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+  console.log(`http://localhost:${port}/`);
+});
+// link between my project and mongooDB
+mongoose
+  .connect(
+    "mongodb+srv://geohousam_db:sHSV85O1kMDD5njC@hegazystart2025.kx8h76l.mongodb.net/all-data?retryWrites=true&w=majority&appName=hegazystart2025"
+  )
+  .then(() => {
+    //express
+    app.get("/", (req, res) => {
+      res.sendFile("./views/home.html", { root: __dirname });
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
